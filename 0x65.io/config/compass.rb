d53fce9c02d70720c0e264bcfc8be4e8445adf8c
ben <ben@ben-laptop.(none)>
2011-06-15 21:34:45 +0800

    hello world

---
+++ 0x65.io/config/compass.rb
@@ -0,0 +1,9 @@
+project_path = Sinatra::Application.root
+environment = :development
+
+# This is common configuration
+sass_dir = File.join 'views', 'sass'
+images_dir = File.join 'static', 'images'
+http_path = "/"
+http_images_path = "/images"
+http_stylesheets_path = "/stylesheets"
